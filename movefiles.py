6fadf8255da7898a69c92ae748e8083b69705f6d
Akansha Jain <akansha.da1@iiitmk.ac.in>
2018-06-14 13:14:30 +0530

    updating for replacement if file already exists

--- movefiles.py
+++ movefiles.py
@@ -11,7 +11,8 @@ def moveFiles(inputfolder, outputfolder):
     infiles = os.listdir(inputfolder)
 
     for f in infiles:
-        shutil.move(inputfolder+f, outputfolder)
+        # shutil.move(inputfolder+f, outputfolder)
+        shutil.move(os.path.join(inputfolder, f), os.path.join(outputfolder, f))
 
 
 if __name__ == '__main__':
